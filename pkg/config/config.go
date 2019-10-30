4140d133cfc8e4992da2fe814422414fd6fd08af
Derek Brown <derek.brown@datadoghq.com>
2019-10-29 17:25:13 -0700

    Removes the trace & process agent from 32 bit windows builds

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"time"
 
@@ -372,7 +373,11 @@ func initConfig(config Config) {
 	// Note that trace-agent environment variables are parsed in pkg/trace/config/env.go
 	// since some of them require custom parsing algorithms. DO NOT add environment variable
 	// bindings here, add them there instead.
-	config.BindEnvAndSetDefault("apm_config.enabled", true)
+	if(runtime.GOARCH == "386") {
+		config.BindEnvAndSetDefault("apm_config.enabled", false)
+	} else {
+		config.BindEnvAndSetDefault("apm_config.enabled", true)
+	}
 
 	// Process agent
 	config.SetDefault("process_config.enabled", "false")
